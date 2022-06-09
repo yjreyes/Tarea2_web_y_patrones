3334b2d404473da91e3f7152c3f939be12dadc66
yervi <yervi@LAPTOP-J3I0N1UB>
2022-06-08 23:33:43 -0600

    Entrega de tarea2, web y patrones

---
+++ src/test/java/com/Tarea2/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
